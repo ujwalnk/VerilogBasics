099ce320e72ff522f69c45c879db0f22525a4282
Ujwal N K <ujwal.nk@gmail.com>
2022-11-18 12:24:53 +0530

    Create MU2X1.v

---
+++ MU2X1.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Madilu
+// Engineer: Ujwal N K
+// 
+// Create Date: 29.09.2022 06:27:32
+// Design Name: MUX (2x1)
+// Module Name: MU2X1
+// Project Name: Multiplexer
+// Target Devices: Xilinx Boolean Spartan7
+// Tool Versions: Vivado 21
+// Description: 2x1 Multiplexer
+// 
+// Dependencies: None
+// 
+// Revision: 1.0
+// Revision 0.01 - File Created
+// Additional Comments: None
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module MU2X1(out, selection, in);
+    output out;
+    input selection;
+    input [0:1]in;
+    
+    assign out = selection? in[1] : in[0];
+endmodule
