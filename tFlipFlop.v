36c068dd83a7825f98fd164926aee633359b6e0c
Ujwal N K <ujwal.nk@gmail.com>
2022-11-22 10:46:37 +0530

    Create tFlipFlop.v

---
+++ tFlipFlop.v
@@ -0,0 +1,29 @@
+timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Madilu
+// Engineer: Ujwal N K
+// 
+// Create Date: 22.11.2022 07:08:54
+// Design Name: toggle Flip Flop
+// Module Name: tFlipFlop
+// Project Name: tFlipFlop
+// Target Devices: Xilinx Boolean Spartan 7
+// Tool Versions: Vivado 21
+// Description: Toggle Flip Flop
+// 
+// Dependencies: dFlipFlop
+// 
+// Revision: 1.0
+// Revision 1.0 - File Created
+// Additional Comments: None
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module tFlipFlop(out, in, clk);
+	output out;
+	input in, clk;
+	wire d
+
+	assign d = t^q;
+	dFlipFlop(q, d, clk);
+endmodule
